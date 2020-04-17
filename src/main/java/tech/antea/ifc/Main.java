d078c70e5a12a73839ad0e8d61dea2337d3acbac
Giovanni Velludo <velludo.giovanni@anteash.com>
2020-04-17 22:04:08 +0200

    added an IfcRepresentationContext to the IfcProject; formatted 2 .eywa files

--- src/main/java/tech/antea/ifc/Main.java
+++ src/main/java/tech/antea/ifc/Main.java
@@ -3,7 +3,12 @@ package tech.antea.ifc;
 import com.buildingsmart.tech.ifc.IfcActorResource.IfcOrganization;
 import com.buildingsmart.tech.ifc.IfcActorResource.IfcPerson;
 import com.buildingsmart.tech.ifc.IfcActorResource.IfcPersonAndOrganization;
+import com.buildingsmart.tech.ifc.IfcGeometryResource.IfcAxis2Placement3D;
+import com.buildingsmart.tech.ifc.IfcGeometryResource.IfcCartesianPoint;
+import com.buildingsmart.tech.ifc.IfcGeometryResource.IfcDirection;
 import com.buildingsmart.tech.ifc.IfcKernel.IfcProject;
+import com.buildingsmart.tech.ifc.IfcRepresentationResource.IfcGeometricRepresentationContext;
+import com.buildingsmart.tech.ifc.IfcRepresentationResource.IfcRepresentationContext;
 import com.buildingsmart.tech.ifc.IfcUtilityResource.IfcApplication;
 import com.buildingsmart.tech.ifc.IfcUtilityResource.IfcChangeActionEnum;
 import com.buildingsmart.tech.ifc.IfcUtilityResource.IfcOwnerHistory;
@@ -15,6 +20,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.TimeZone;
 
 public class Main {
@@ -23,40 +30,57 @@ public class Main {
         IfcProject project = new IfcProject();
         String filePath = "./ifc-out/cylinder-test.ifcXML";
 
-        IfcPerson johnDoe = new IfcPerson();
-        johnDoe.setGivenName("John");
-        johnDoe.setFamilyName("Doe");
+        project.setGlobalId("0NZgqcQj5AlvVyooSuXX4r");
+        // TODO: implement algorithm for GlobalId generation as described in
+        //  the IFC specification
 
-        IfcOrganization antea = new IfcOrganization();
-        antea.setIdentification("Antea");
-        antea.setName("Antea Holding S.R.L.");
+        IfcPerson thePerson = new IfcPerson();
+        thePerson.setGivenName("John");
+        thePerson.setFamilyName("Doe");
+
+        IfcOrganization theOrganization = new IfcOrganization();
+        theOrganization.setIdentification("Antea");
+        theOrganization.setName("Antea S.R.L.");
 
         IfcPersonAndOrganization owningUser = new IfcPersonAndOrganization();
-        owningUser.setThePerson(johnDoe);
-        owningUser.setTheOrganization(antea);
+        owningUser.setThePerson(thePerson);
+        owningUser.setTheOrganization(theOrganization);
 
         IfcApplication owningApplication = new IfcApplication();
-        owningApplication.setApplicationDeveloper(antea);
+        owningApplication.setApplicationDeveloper(theOrganization);
         owningApplication.setApplicationFullName("AnteaIFC 0.1");
         owningApplication.setApplicationIdentifier("AnteaIFC");
         owningApplication.setVersion("0.1");
 
         IfcOwnerHistory ownerHistory = new IfcOwnerHistory();
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
-        int creationTime = (int) (calendar.getTimeInMillis() / 1000);
+        int creationTime = (int) (calendar.getTimeInMillis() / 1000); // seconds
         ownerHistory.setOwningUser(owningUser);
         ownerHistory.setOwningApplication(owningApplication);
         ownerHistory.setChangeAction(IfcChangeActionEnum.NOCHANGE);
         ownerHistory.setCreationDate(creationTime);
 
-        project.setGlobalId("0NZgqcQj5AlvVyooSuXX4r");
-        // TODO: implement algorithm for GlobalId creation as described in
-        //  the IFC specification
         project.setOwnerHistory(ownerHistory);
         project.setName("cylinder-test");
-        //project.setUnitsInContext();
-        // TODO: add method IfcContext.setRepresentationContexts() to the IFC
-        //  .JAVA library
+
+        IfcGeometricRepresentationContext reprContext =
+                new IfcGeometricRepresentationContext();
+        reprContext.setContextIdentifier("Plan");
+        reprContext.setContextType("Model");
+        reprContext.setCoordinateSpaceDimension(3);
+        reprContext.setPrecision(1.E-5);
+        IfcAxis2Placement3D worldCoordSys = new IfcAxis2Placement3D();
+        worldCoordSys.setLocation(new IfcCartesianPoint(new Double[]{0.0, 0.0, 0.0}));
+        // TODO: edit library and add check on the size of the array passed in the constructor of IfcCartesianPoint and IfcDirection?
+        worldCoordSys.setAxis(new IfcDirection(new Double[]{0.0, 0.0, 1.0}));
+        worldCoordSys.setRefDirection(new IfcDirection(new Double[]{1.0, 0.0, 0.0}));
+        reprContext.setWorldCoordinateSystem(worldCoordSys);
+        reprContext.setTrueNorth(new IfcDirection(new Double[]{0.0, 1.0, 0.0}));
+        Set<IfcRepresentationContext> reprContexts = new HashSet<>();
+        reprContexts.add(reprContext);
+
+        project.setRepresentationContexts(reprContexts);
+//        project.setUnitsInContext();
 
         writeModelToXML(project, filePath);
     }
